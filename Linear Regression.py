480655f017ba8e0ad1e9eeab8e44bea66756e3dc
J vdB <jjvdb@users.noreply.github.com>
2019-01-01 20:41:14 +0000

    Updating the code to use Numpy arrays

--- Linear Regression.py
+++ Linear Regression.py
@@ -3,8 +3,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 # declare the Linear Regression class
-class Linear_Regression:
+class LinearRegression:
     def __init__(self, x_values, y_values):
         """
         Function to initialize the LG object, taking in two arrays - the x values and the y values
@@ -21,12 +22,9 @@ class Linear_Regression:
             """
             Function to calculate the mean of the x and y datasets
             """
-            sum_x = 0
-            sum_y = 0
 
-            for i in range(0, self.x_values.shape[0]):
-                sum_x = sum_x + self.x_values[i]
-                sum_y = sum_y + self.y_values[i]
+            sum_x = np.sum(self.x_values)
+            sum_y = np.sum(self.y_values)
 
             x_mean = sum_x / self.x_values.shape[0]
             y_mean = sum_y / self.y_values.shape[0]
@@ -36,14 +34,10 @@ class Linear_Regression:
         # get the means of the x and y datasets
         x_mean, y_mean = calculate_means()
 
-        # declare the numerator and denominator used to calculate the gradient of the line of best fit
-        numerator = 0
-        denominator = 0
-
+        # declare the numerator and denominator used to calculate the gradient of the line of best fit and
         # calculate the gradient of the line of best fit
-        for i in range(0, self.x_values.shape[0]):
-            numerator = numerator + ((self.y_values[i] - y_mean) * (self.x_values[i] - x_mean))
-            denominator = denominator + ((self.x_values[i] - x_mean) ** 2)
+        numerator = np.sum((self.y_values - y_mean) * (self.x_values - x_mean))
+        denominator = np.sum((self.x_values - x_mean) ** 2)
 
         # calculate the gradient of the line of best fit
         gradient = numerator / denominator
@@ -51,11 +45,8 @@ class Linear_Regression:
         # calculate the intercept of the line of best fit
         intercept = y_mean - gradient * x_mean
 
-        # declare the array of the y-coordinates of the line of best fit
-        best_fit = np.zeros(self.y_values.shape[0])
-
-        for i in range(0, len(self.x_values)):
-            best_fit[i] = (intercept + gradient * self.x_values[i])
+        # calculate the array of the y-coordinates of the line of best fit
+        best_fit = (intercept + gradient * self.x_values)
 
         # create a 2D plot of the regression vs the actual data
         # see here for symbol documentation in matplotlib: https://matplotlib.org/api/markers_api.html
@@ -76,7 +67,7 @@ x = np.array([63, 64, 66, 69, 69, 71, 71, 72, 73, 75])
 y = np.array([127, 121, 142, 157, 162, 156, 169, 165, 181, 208])
 
 # instantiate the Linear Regression object
-LR = Linear_Regression(x, y)
+LR = LinearRegression(x, y)
 
 # calculate the line of best fit of the x and y coordinate data
 regression = LR.regression()
