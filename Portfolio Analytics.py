7f2ae4b863767ef84bbdaf28ca51b19a8a7456c6
J vdB <jjvdb@users.noreply.github.com>
2019-01-02 03:43:59 +0000

    Update Portfolio Analytics.py
    Adding R-Squared functionality.

--- Portfolio Analytics.py
+++ Portfolio Analytics.py
@@ -2,56 +2,61 @@ import numpy as np
 from scipy import integrate
 import matplotlib.pyplot as plt
 
-def Portfolio_Returns(portfolio, weights):
+
+def portfolio_returns(returns, weights):
     """
     Calculate the total portfolio returns time-series array by multiplying the weights matrix (dimensions = 1*N) and the
     portfolio matrix (dimensions = N*t), for N securities in the portfolio and t returns per security. This function
     returns a 1*N matrix where each element in the matrix is the portfolio return fo a given time.
 
-    :param portfolio: np.array([float])
+    :param returns: np.array([float])
     :param weights: np.array([float])
     :return: np.array([float])
     """
 
     # the portfolio returns are given by the dot product of the weights matrix and the portfolio matrix
-    portfolio_returns = np.dot(weights, portfolio)
+    port_returns = np.dot(weights, returns)
 
-    return portfolio_returns
+    return port_returns
 
-def Alpha(portfolio_returns, risk_free_rate, market_returns, beta):
+
+def alpha(port_returns, risk_free_rate, market_returns, B):
     """
     Calculate the Alpha of the portfolio.
 
-    :param portfolio_returns: np.array([float])
+    :param port_returns: np.array([float])
     :param risk_free_rate: np.array([float])
     :param market_returns: np.array([float])
-    :param beta: float
+    :param B: float
     :return: float
     """
 
     # the portfolio Alpha is given by the below equation, as stated by the Capital Asset Pricing Model
-    alpha = sum(portfolio_returns - risk_free_rate + beta*(market_returns - risk_free_rate))
+    A = np.sum(port_returns - risk_free_rate + B*(market_returns - risk_free_rate))
 
-    return alpha
+    return A
 
-def Beta(portfolio_returns, market_returns):
+
+def beta(port_returns, market_returns):
     """
     Calculate the Beta of the portfolio.
 
-    :param portfolio_returns: np.array([float])
+    :param port_returns: np.array([float])
     :param market_returns: np.array([float])
     :return: float
     """
 
     # the portfolio Beta is given by the covariance of the returns of the portfolio and the returns of the market,
     # divided by the variance of the returns of the market
-    beta = np.cov(np.array([portfolio_returns, market_returns]))[0][1]/np.cov([portfolio_returns, market_returns])[1][1]
+    B = np.cov(np.array([port_returns, market_returns]))[0][1]/np.cov([port_returns, market_returns])[1][1]
 
-    return beta
+    return B
 
-def Portfolio_Volatility(returns, weights):
+
+def portfolio_volatility(returns, weights):
     """
-    Calculate the total portfolio volatility (the variance of the returns of the portfolio) using a covariance matrix.
+    Calculate the total portfolio volatility (the variance of the historical returns of the portfolio) using a
+    covariance matrix.
 
     :param returns: np.array([float])
     :param weights: np.array([float])
@@ -65,15 +70,16 @@ def Portfolio_Volatility(returns, weights):
     covariance = np.cov(returns)
 
     # calculate the portfolio volatility
-    portfolio_volatility = np.dot(np.dot(weights, covariance), W_T)[0]
+    port_volatility = np.dot(np.dot(weights, covariance), W_T)[0]
 
-    return portfolio_volatility
+    return port_volatility
 
-def Sharpe_Ratio(portfolio_returns, risk_free_rate, asset_returns, weights):
+
+def sharpe_ratio(port_returns, risk_free_rate, asset_returns, weights):
     """
     Calculate the Sharpe ratio of the portfolio.
 
-    :param portfolio_returns: np.array([float])
+    :param port_returns: np.array([float])
     :param risk_free_rate: float
     :param asset_returns: np.array([float])
     :param weights: np.array([float])
@@ -81,14 +87,30 @@ def Sharpe_Ratio(portfolio_returns, risk_free_rate, asset_returns, weights):
     """
 
     # calculate the standard deviation of the returns of the portfolio
-    portfolio_standard_deviation = np.sqrt(Portfolio_Volatility(asset_returns, weights))
+    portfolio_standard_deviation = np.sqrt(portfolio_volatility(asset_returns, weights))
 
     # calculate the Sharpe ratio of the portfolio
-    sharpe_ratio = sum((portfolio_returns - risk_free_rate))/portfolio_standard_deviation
+    sr = np.sum((port_returns - risk_free_rate))/portfolio_standard_deviation
 
-    return sharpe_ratio
+    return sr
 
-def Analytical_VaR(returns, weights, a, n):
+
+def r_squared(port_returns, market_returns):
+    """
+    Calculate the R-Squared value of the portfolio.
+
+    :param port_returns: np.array([float])
+    :param market_returns: np.array([float])
+    :return: float
+    """
+
+    # calculate the R-Squared value of the portfolio using the standard statistical definition of R-Squared
+    rs = 1 - np.var(port_returns-market_returns)/np.var(market_returns)
+
+    return rs
+
+
+def analytical_var(returns, weights, a, n):
     """
     Calculate the Analytical VaR for a portfolio comprised of returns.shape[0] assets. This takes in several parameters.
     The first is a numpy array - returns, where each row corresponds to the historical daily returns (given as a decimal
@@ -105,13 +127,10 @@ def Analytical_VaR(returns, weights, a, n):
     """
 
     # calculate the standard deviation of the portfolio
-    sigma = np.sqrt(Portfolio_Volatility(returns, weights))
-
-    # calculate the mean return of each asset
-    mean_asset_returns = [np.sum(x)/len(x) for x in returns]
+    sigma = np.sqrt(portfolio_volatility(returns, weights))
 
     # calculate the mean return of the portfolio
-    mu = np.sum(mean_asset_returns)/returns.shape[0]
+    mu = np.sum(portfolio_returns(returns, weights))/returns.shape[1]
 
     # set the plot range at 9 standard deviations from the mean in both the left and right directions
     plot_range = 9 * sigma
@@ -132,14 +151,15 @@ def Analytical_VaR(returns, weights, a, n):
     risk_range = (np.abs(x - a)).argmin()
 
     # use Simpsons rule to estimate the cdf of the Normal distribution up to a%
-    Analytical_VaR = integrate.simps(pdf[0:risk_range], x[0:risk_range])
+    a_var = integrate.simps(pdf[0:risk_range], x[0:risk_range])
 
     # print the Analytical VaR
-    print('Analytical VaR = ' + str(Analytical_VaR*100) + '% at ' + str(x[risk_range]*100) + '% of daily returns')
+    print('Analytical VaR = ' + str(a_var * 100) + '% at ' + str(x[risk_range] * 100) + '% of daily returns')
 
-    return x, pdf, risk_range, Analytical_VaR
+    return x, pdf, risk_range, a_var
 
-def Historical_VaR(returns, weights, a):
+
+def historical_var(returns, weights, a):
     """
     Calculate the Historical VaR for a portfolio comprised of returns.shape[0] assets. This takes in several parameters.
     The first is a numpy array - returns, where each row corresponds to the historical daily returns (given as a decimal
@@ -153,36 +173,36 @@ def Historical_VaR(returns, weights, a):
     :return: np.array([float]), float
     """
 
-    # generate the transform of the 1D numpy weights array
-    W_T = [[x] for x in weights]
-
-    # calculate the weighted time-series of portfolio returns
-    portfolio_returns = np.sum(np.multiply(returns, W_T), axis=0)
+    # calculate the total portfolio returns
+    port_returns = portfolio_returns(returns, weights)
 
     # calculate the Historical VaR of the portfolio - check if the daily return value is less than a% - if it is, then
     # it is counted in the Historical VaR calculation
     relevant_returns = 0
-    for i in range(0, portfolio_returns.shape[0]):
-        if portfolio_returns[i] < a:
+    for i in range(0, port_returns.shape[0]):
+        if port_returns[i] < a:
             relevant_returns += 1
 
-    Historical_VaR = relevant_returns/portfolio_returns.shape[0]
+    h_var = relevant_returns/port_returns.shape[0]
 
     # print the Historical VaR
-    print('Historical VaR = ' + str(Historical_VaR * 100) + '% at ' + str(a * 100) + '% of daily returns')
+    print('Historical VaR = ' + str(h_var * 100) + '% at ' + str(a * 100) + '% of daily returns')
 
-    return portfolio_returns, Historical_VaR;
+    return h_var
 
-def plot_Analytical_VaR(x, pdf, a):
+
+def plot_analytical_var(x, pdf, a):
     """
     Plot the normal distribution of portfolio returns - show the area under the normal curve that corresponds to the
-    Analytical VaR of the portfolio.
+    Analytical VaR of the portfolio. The variable x is the range of daily returns of the portfolio (the x-axis), pdf
+    is the Normal Distribution of the historical returns of the portfolio (the y-axis), and a is the cutoff value.
 
     :param x: np.array([float])
     :param pdf: np.array([float])
     :param a: float
     :return:
     """
+
     plt.plot(x, pdf, linewidth=2, color='r', label='Distribution of Returns')
     plt.fill_between(x[0:a], pdf[0:a], facecolor='blue', label='Analytical VaR')
     plt.legend(loc='upper left')
@@ -191,21 +211,19 @@ def plot_Analytical_VaR(x, pdf, a):
     plt.title('Frequency vs Daily Returns')
     plt.show()
 
-    return;
 
-# instead of using a defined bin width, see if there is a built-in function to plot the frequency of each element of the
-# returns. Pass an optional argument (bin_width = False) to allow the user to choose if they want to group returns into
-# bins
-def plot_Historical_VaR(x, a, bins):
+def plot_historical_var(x, a, bins):
     """
     Plot a histogram showing the distribution of portfolio returns - mark the cutoff point that corresponds to the
-    Historical VaR of the portfolio.
+    Historical VaR of the portfolio. The variable x is the historical distribution of returns of the portfolio, a is
+    the cutoff value, and the bins are the bins in which to stratify the historical returns.
 
     :param x: np.array([float])
     :param a: float
     :param bins: np.array([float])
     :return:
     """
+
     plt.hist(x, bins, label='Distribution of Returns')
     plt.axvline(x=a, ymin=0, color='r', label='Historical VaR cutoff point')
     plt.legend(loc='upper left')
@@ -214,7 +232,6 @@ def plot_Historical_VaR(x, a, bins):
     plt.title('Frequency vs Daily Returns')
     plt.show()
 
-    return;
 
 # declare the weight of each asset in the portfolio - each element in the row is the weight of the nth asset
 W = np.array([0.25, 0.15, 0.2, 0.3, 0.05, 0.05])
@@ -229,50 +246,53 @@ X = np.array([[0.1, 0.3, 0.4, 0.8, 0.9],
               [0.32, 0.24, 0.24, 0.1, 0.55]])
 
 # calculate the returns of the portfolio
-portfolio_returns = Portfolio_Returns(X, W)
-
-print('Historical Portfolio Returns = ' + str(portfolio_returns))
+port_returns = portfolio_returns(X, W)
 
 # declare the historical returns of the benchmark index
 market_returns = np.array([0.8,1.9,0.1003,10.9,0.1])
 
-# calculate the
-beta = Beta(portfolio_returns, market_returns)
+# calculate the Beta of the portfolio
+B = beta(port_returns, market_returns)
 
-print('Portfolio Beta = ' + str(beta))
+print('Historical Portfolio Returns = ' + str(port_returns))
 
-print('Portfolio Volatility = ' + str(Portfolio_Volatility(portfolio_returns, W)))
+print('Portfolio R-Squared = ' + str(r_squared(port_returns, market_returns)))
 
-print('Portfolio Alpha = ' + str(Alpha(portfolio_returns, 0.02, market_returns, beta)))
+print('Portfolio Beta = ' + str(B))
 
-print('Portfolio Sharpe Ratio = ' + str(Sharpe_Ratio(portfolio_returns, 0.02, X, W)))
+print('Portfolio Volatility = ' + str(portfolio_volatility(port_returns, W)))
+
+print('Portfolio Alpha = ' + str(alpha(port_returns, 0.02, market_returns, B)))
+
+print('Portfolio Sharpe Ratio = ' + str(sharpe_ratio(port_returns, 0.02, X, W)))
 
 # calculate the Analytical VaR at -5% daily returns and the associated values
-x, pdf, a, A_VaR = Analytical_VaR(X, W, -0.05, 100000)
+x, pdf, a, A_VaR = analytical_var(X, W, -0.05, 100000)
 
 # plot the Analytical VaR
-plot_Analytical_VaR(x, pdf, a)
+plot_analytical_var(x, pdf, a)
 
 # calculate the overall historical portfolio returns, and the Historical VaR at 50% daily returns
-portfolio_returns, H_VaR = Historical_VaR(X, W, 0.5)
+H_VaR = historical_var(X, W, 0.5)
 
 # plot the Historical VaR
 bins = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
-plot_Historical_VaR(portfolio_returns, 0.5, bins)
+plot_historical_var(port_returns, 0.5, bins)
 
 # perform unit testing to verify the values of the Analytical and Historical VaR
 import unittest
 
-class VaR_Testing(unittest.TestCase):
+
+class VaRTesting(unittest.TestCase):
     # define the VaR testing function
     def test_VaR(self):
         # calculate the Historical VaR
-        _, H_VaR = Historical_VaR(X, W, 0.5)
+        H_VaR = historical_var(X, W, 0.5)
         # calculate the Analytical VaR
-        _, _, _, A_VaR = Analytical_VaR(X, W, -0.05, 100000)
+        _, _, _, A_VaR = analytical_var(X, W, -0.05, 100000)
         # test the Historical VaR - confirm that it has the correct value
         self.assertEqual(H_VaR, 0.4)
         # test the Analytical VaR - confirm that it has the correct value to 4 decimal places
-        self.assertEqual(round(A_VaR, 4), 0.1237)
+        self.assertEqual(round(A_VaR, 4), 0.0805)
 
 unittest.main()
