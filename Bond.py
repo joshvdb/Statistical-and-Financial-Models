0515ccd98c3edcd4f5418933dda12af90511a4c6
J vdB <jjvdb@users.noreply.github.com>
2019-01-14 05:56:11 +0000

    Update Bond.py

--- Bond.py
+++ Bond.py
@@ -31,23 +31,23 @@ class Bond:
         and Modified Duration of a bond.
         """
 
-        def f_YTM(YTM):
+        def f_ytm(ytm):
             """
             Define the bond pricing formula with YTM as the independent variable, with price moved to the right-hand side,
             setting the equation to zero.
             This allows us to find the root - the YTM.
             """
             C = (self.coupon * 0.01 * self.face_value)/self.num_coupon_periods
-            return ((C * (1 - (1 / ((1 + YTM) ** self.n)))) / YTM) + (self.face_value / ((1 + YTM) ** self.n)) - self.price;
+            return ((C * (1 - (1 / ((1 + ytm) ** self.n)))) / ytm) + (self.face_value / ((1 + ytm) ** self.n)) - self.price;
 
-        def f_YTC(YTC):
+        def f_ytc(ytc):
             """
             Define the bond pricing formula with YTC as the independent variable, with price moved to the right-hand side,
             setting the equation to zero.
             This allows us to find the root - the YTC.
             """
             C = self.coupon*0.01*self.face_value
-            return (C / 2)*((1 - (1 + YTC/2) ** (-(2 * self.t)))/(YTC/2)) + self.call_price/((1 + YTC/2)**(2 * self.t)) - self.price;
+            return (C / 2)*((1 - (1 + ytc/2) ** (-(2 * self.t)))/(ytc/2)) + self.call_price/((1 + ytc/2)**(2 * self.t)) - self.price;
 
         def calculate_yields():
             """
@@ -60,32 +60,32 @@ class Bond:
 
             # calculate the functions based on each YTM and YTC value - we then need to find the value closest to 0
             # (that is, where the price calculated from the yield = the actual bond price)
-            y1 = f_YTM(x1)
-            y2 = f_YTC(x1)
+            y1 = f_ytm(x1)
+            y2 = f_ytc(x1)
 
             # find the value in x1 that is closest to 0 - start at the index: initial = 0
-            initial_YTM = 0
-            initial_YTC = 0
+            initial_ytm= 0
+            initial_ytc = 0
 
             # search through the values of f_YTM(x), f_YTC(x) - the smallest absolute value (closest to zero) is the
             # best approximation for the actual YTM and YTC
             for i in range(0, len(y1)):
                 # check if the current YTM value is a better approximation
-                if abs(y1[initial_YTM]) > abs(y1[i]):
-                    initial_YTM = i
+                if abs(y1[initial_ytm]) > abs(y1[i]):
+                    initial_ytm = i
                 # check if the current YTC value is a better approximation
-                if abs(y2[initial_YTC]) > abs(y2[i]):
-                    initial_YTC = i
+                if abs(y2[initial_ytc]) > abs(y2[i]):
+                    initial_ytc = i
 
             # calculate the YTM and YTC values based on the approximations above
-            YTM = self.num_coupon_periods*x1[initial_YTM]
-            YTC = x1[initial_YTC]
+            ytm = self.num_coupon_periods*x1[initial_ytm]
+            ytc = x1[initial_ytc]
 
             # calculate the YTW value - the minimum of the YTM and YTC
-            YTW = min(YTM, YTC)
+            ytw = min(ytm, ytc)
 
             # return the YTM, YTC and YTW of the bond
-            return YTM, YTC, YTW;
+            return ytm, ytc, ytw;
 
         def calculate_durations(ytm):
             """
@@ -96,15 +96,15 @@ class Bond:
             # calculate the coupon of the bond
             i = self.coupon * 0.01
             # calculate the periodic coupon payment of the bond
-            C = self.coupon * 0.01 * self.face_value
+            c = self.coupon * 0.01 * self.face_value
 
             # calculate the present value of the bond
-            PV = 0
+            pv = 0
             for t in range(1, self.n + 1, 1):
-                PV += ((t * C) / ((1 + i) ** t))
+                pv += ((t * c) / ((1 + i) ** t))
 
             # calculate the Macaulay Duration of the bond
-            macaulay_duration = (PV + ((self.n * self.face_value) / ((1 + i) ** t))) / self.price
+            macaulay_duration = (pv + ((self.n * self.face_value) / ((1 + i) ** t))) / self.price
 
             # calculate the Modified Duration of the bond
             modified_duration = macaulay_duration/(1 + ytm/self.num_coupon_periods)
@@ -113,24 +113,17 @@ class Bond:
             return macaulay_duration, modified_duration;
 
         # calculate the YTM, YTC and YTW of the bond
-        YTM, YTC, YTW = calculate_yields()
+        ytm, ytc, ytw = calculate_yields()
 
         # calculate the Macaulay Duration and Modified Duration of the bond
-        macaulay_duration, modified_duration = calculate_durations(YTM)
+        macaulay_duration, modified_duration = calculate_durations(ytm)
 
         # print the values of the YTM, YTC, YTW, Macaulay Duration and Modified Duration of the bond
-        print ('Yield to Maturity (Annual) = {0:.2f}%'.format((YTM * 100)))
-        print('Yield to Call (Annual) = {0:.2f}%'.format((YTC * 100)))
-        print('Yield to Worst (Annual) = {0:.2f}%'.format((YTW * 100)))
+        print('Yield to Maturity (Annual) = {0:.2f}%'.format((ytm * 100)))
+        print('Yield to Call (Annual) = {0:.2f}%'.format((ytc * 100)))
+        print('Yield to Worst (Annual) = {0:.2f}%'.format((ytw * 100)))
         print('Macaulay Duration = {0:.2f} years'.format(macaulay_duration))
         print('Modified Duration = {0:.2f} years'.format(modified_duration))
 
         # return the values of the YTM, YTC, YTW, Macaulay Duration and Modified Duration of the bond
-        return YTM, YTC, YTW, macaulay_duration, modified_duration;
-
-# instantiate a bond with market price = $1000, annual coupon = 5%, call price = $1100, face value = $1000, years left until
-# the call date = 5, years remaining until maturity = 5 and number of coupon payments per year = 2
-bond = Bond(1000, 5, 1100, 1000, 5, 5, 1)
-
-# run analytics on the bond
-bond.calculate_bond_analytics()
+        return ytm, ytc, ytw, macaulay_duration, modified_duration;
