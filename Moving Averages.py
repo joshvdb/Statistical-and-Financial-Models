74cfe41674df951d4659aa9f175968eaf92768e6
J vdB <jjvdb@users.noreply.github.com>
2019-01-14 05:51:27 +0000

    Update Moving Averages.py
    Re-writing in line with PEP 8

--- Moving Averages.py
+++ Moving Averages.py
@@ -2,7 +2,7 @@ import numpy as np
 from numba import jit
 
 @jit(nopython = True, cache = False)
-def SMA(matrix, interval):
+def sma(matrix, interval):
     """
     Function to implement a Simple Moving Average, optimized with Numba.
     :param matrix: np.array([float])
@@ -11,16 +11,16 @@ def SMA(matrix, interval):
     """
 
     # declare empty SMA numpy array
-    sma = np.zeros((matrix.shape[0] - interval))
+    s = np.zeros((matrix.shape[0] - interval))
 
     # calculate the value of each point in the Simple Moving Average array
-    for t in range(0, sma.shape[0]):
-        sma[t] = np.sum(matrix[t:t + interval])/interval
+    for t in range(0, s.shape[0]):
+        s[t] = np.sum(matrix[t:t + interval])/interval
 
-    return sma
+    return s
 
 @jit(nopython = True, cache = False)
-def EMA(matrix, alpha):
+def ema(matrix, alpha):
     """
     Function to implement an Exponential Moving Average, optimized with Numba. The variable alpha represents the degree
     of weighting decrease, a constant smoothing factor between 0 and 1. A higher alpha discounts older observations faster.
@@ -30,16 +30,16 @@ def EMA(matrix, alpha):
     """
 
     # declare empty EMA numpy array
-    ema = np.zeros(matrix.shape[0])
+    e = np.zeros(matrix.shape[0])
 
     # set the value of the first element in the EMA array
-    ema[0] = matrix[0]
+    e[0] = matrix[0]
 
     # use the EMA formula to calculate the value of each point in the EMA array
     for t in range(1, matrix.shape[0]):
-        ema[t] = alpha*matrix[t] + (1 - alpha)*ema[t - 1]
+        e[t] = alpha*matrix[t] + (1 - alpha)*e[t - 1]
 
-    return ema
+    return e
 
 # declare SMA and EMA variables
 interval = 2
@@ -47,5 +47,5 @@ alpha = 0.1
 matrix = np.array([1.5, 1.8, 1.9, 2.2, 1.4, 1.8, 1.95, 1.87, 1.88, 1.89])
 
 # print results
-print(EMA(matrix, alpha))
-print(SMA(matrix, interval))
+print(ema(matrix, alpha))
+print(sma(matrix, interval))
